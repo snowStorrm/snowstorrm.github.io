57bf35a3e0dde781d0ddefd7c8d1f974ef8bbdb7
Ryan Zucker <ryanjz101@gmail.com>
2021-10-22 11:25:47 -0400

    added visuals for quarks

--- projects/radiant/radiant.js
+++ projects/radiant/radiant.js
@@ -1,12 +1,25 @@
 
+let quarks = [];
 function setup() {
+    colorMode(HSB);
     createCanvas(windowWidth, windowHeight);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
-
+    for (let i of quarks) {
+        i.update();
+        if (i.vis = 0) {
+            let index = quarks.indexOf(i);
+            quarks.splice(index, 1);
+            continue;
+        }
+        i.show();
+    }
+}
+function mousePressed() {
+    quarks.push(new outQuark(createVector(random(0,width), height), 10);
 }
 
 class outQuark {
@@ -31,13 +44,7 @@ class outQuark {
         this.acc.mult(0);
     }
     show() {
+        stroke(map(this.vis, 0, 255, 60, -235), 255, this.vis);
         ellipse(this.pos.x, this.pos.y, this.size, this.size);
-    }
-}
-class inQuark {
-    constructor(origin, size) {
-        this.pos = origin;
-        this.vel = createVector();
-        this.size = size;
     }
 }
