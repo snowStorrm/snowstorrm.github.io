243cd202996e7b991300ff35e9327b28ee39f344
snowStorrm <84199708+snowStorrm@users.noreply.github.com>
2023-01-10 09:30:48 -0500

    Delete projects directory

--- projects/radiant/menuRight.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    colorMode(HSB, 255);
-    let canvas = createCanvas(windowWidth * 0.25, windowHeight);
-    canvas.position(windowWidth * 0.75, 0);
-}
-function windowResized() {
-    let canvas = resizeCanvas(windowWidth * 0.25, windowHeight);
-    canvas.position(windowWidth * 0.75, 0);
-}
